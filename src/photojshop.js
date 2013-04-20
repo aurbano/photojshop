2e089eaa9fbfc09866e7df3e979bbfa8b71dbe89
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-04-20 11:28:28 +0200

    Initial commit

---
+++ src/photojshop.js
@@ -0,0 +1,8 @@
+/**
+ * PhotoJShop
+ * A JavaScript library to perform image editing using filters similar to those found
+ * in most photo editing software.
+ *
+ * @author Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
+ * @version 1
+ */
